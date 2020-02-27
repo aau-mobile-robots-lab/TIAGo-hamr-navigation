abd0b327907a0ab35dd65edd044f2484e75b6e99
martin.jensen37 <martin.jensen37@gmail.com>
2020-02-27 16:21:16 +0100

    Add PotentialField.py that can compute the potential field of an obstacle given position and size.

--- MPC_Differential_Drive/src/PotentialField.py
+++ MPC_Differential_Drive/src/PotentialField.py
@@ -7,9 +7,9 @@ from nav_msgs.msg import Path
 from geometry_msgs.msg import Twist
 from sensor_msgs.msg import Imu
 # Standard python modules
-from mayavi import mlab
+#from mayavi import mlab
 
-import timeit
+import time
 import math
 import numpy as np
 import pandas as pd
@@ -23,17 +23,18 @@ def generatePotentialField(obsPos):
     i, j = map_size
     p = np.zeros(map_size)
     pmax = 255
-    k = 0.04  # Tuning parameter
+    k = 0.1  # Tuning parameter
 
     for obs in range(len(obsPos[:])):
         for ls in range(i):
             for ks in range(j):
                 # print('values of array: ', (ls, ks))
 
-                g = (obsPos[obs][0] - obsPos[obs][2]/2 - ls) + abs(obsPos[obs][0] - obsPos[obs][2]/2 - ls) + \
-                            (ls - obsPos[obs][0]-obsPos[obs][2]/2 + 1) + abs(ls - obsPos[obs][0]-obsPos[obs][2]/2 + 1) +\
-                            (obsPos[obs][1] - obsPos[obs][2]/2 - ks) + abs(obsPos[obs][1] - obsPos[obs][2]/2 - ks) + \
-                            (ks - obsPos[obs][1]-obsPos[obs][2]/2 + 1) + abs(ks - obsPos[obs][1]-obsPos[obs][2]/2 + 1)
+                #g = (obsPos[obs][0] - obsPos[obs][2]/2 - ls) + abs(obsPos[obs][0] - obsPos[obs][2]/2 - ls) + \
+                            #(ls - obsPos[obs][0]-obsPos[obs][2]/2 + 1) + abs(ls - obsPos[obs][0]-obsPos[obs][2]/2 + 1) +\
+                            #(obsPos[obs][1] - obsPos[obs][2]/2 - ks) + abs(obsPos[obs][1] - obsPos[obs][2]/2 - ks) + \
+                            #(ks - obsPos[obs][1]-obsPos[obs][2]/2 + 1) + abs(ks - obsPos[obs][1]-obsPos[obs][2]/2 + 1)
+                g = abs(math.sqrt((obsPos[obs][0]-ls)**2 + (obsPos[obs][1]-ks)**2) - obsPos[obs][2])
                 p[ls, ks] = pmax/(1+math.exp(g*k)) + p[ls, ks]  # (1+g[ls,ks])
 
 
@@ -41,7 +42,7 @@ def generatePotentialField(obsPos):
 
     # Plot the obstacles in 3d and 2d
     fig2, ax1 = plt.subplots(figsize=(4, 4))
-    ax1.imshow(p, cmap='Blues', interpolation='nearest')
+    #ax1.imshow(p, cmap='Blues', interpolation='nearest')
     fig1 = plt.figure()
     ax = fig1.gca(projection='3d')
     ax.set_xlim([0, 500])
@@ -52,12 +53,9 @@ def generatePotentialField(obsPos):
     ax.plot_surface(X, Y, p)
     #ax.plot_surface(X, Y, p[:, :, 1])
     ax.view_init(azim=60, elev=16)
-    fig1.show()
-    plt.show()
+    #fig1.show()
+    #plt.show()
 
-    fig = mlab.figure()
-    surf3 = mlab.surf(X, Y, p, colormap='Oranges')
-    mlab.show()
 
 
 
@@ -71,11 +69,12 @@ def getGlobalPlan():
 
 
 def main():
+    t1 = time.time()
     goal, obsPos = getGlobalPlan()
 
     generatePotentialField(obsPos)
-
-
+    t2 = time.time()
+    print(t2 - t1)
 
 
 
--- MPC_Differential_Drive/src/casadiMPC.py
+++ MPC_Differential_Drive/src/casadiMPC.py
@@ -2,11 +2,12 @@
 import casadi as ca
 import casadi.tools
 # ROS modules
-import rospy
-from nav_msgs.msg import Path
+#import rospy
+#from nav_msgs.msg import Path
 from geometry_msgs.msg import Twist
 from sensor_msgs.msg import Imu
 # Standard python modules
+import time
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -25,25 +26,24 @@ Globalpath = []
    #     getplan[0].unregister()
 
     #Simply add whatever manipulations you want to path_data
-def generatePotentialField():
+#def generatePotentialField():
+    #a = 1+1
 
+#def getGlobalPlan():
 
-
-def getGlobalPlan():
-
-    pathmsg = rospy.wait_for_message("/move_base/GlobalPlanner/plan", Path, timeout=10 )
+    #pathmsg = rospy.wait_for_message("/move_base/GlobalPlanner/plan", Path, timeout=10 )
 
     # rospy.Subscriber("/mobile_base_controller/cmd_vel", Twist, pathcb)
     #getplan = None
     #getplan = rospy.Subscriber("/move_base/GlobalPlanner/plan", Path, pathcb, [getplan])
     #rospy.spin()
-    return pathmsg
+    #return pathmsg
 
 
 def main():
-    rospy.init_node('local_planner')
+    #rospy.init_node('local_planner')
 
-
+    t1 = time.time()
     # State vector
     x = ca.tools.struct_symMX([ca.tools.entry("p", shape=2), "theta"])
 
@@ -66,11 +66,11 @@ def main():
     f = ca.Function('f', [x, u], [rhs], ['x', 'u'], ['dx/dt'])
 
     # Define obstacle
-    obsPos = [0.5, 0]
-    obsRadius = 0.2
+    obsPos = [[0.5, 0, 0.2], [0.5, -0.5, 0.1],[0.3, -0.3, 0.2]]
+
 
     # Define goal
-    goal = [0.8, 0.1]
+    goal = [0.75, -0.75]
 
 
 
@@ -92,7 +92,7 @@ def main():
     opti = ca.Opti()  # casADi optimization object
 
     # Optimization horizon
-    N = 50
+    N = 300
 
     # Decision variables for states and inputs
     X = opti.variable(x.size, N+1)
@@ -125,18 +125,18 @@ def main():
     e_y = (goal[1] - p_y)
 
     s = opti.variable(N)
-
-    d = ca.sqrt((obsPos[0] - p_x)**2 + (obsPos[1] - p_y)**2)
-
     # potential = ca.sqrt(1 / (1 + ca.exp(5 * (d - 0.2))))
 
-    opti.subject_to(d[0:-1].T >= s + obsRadius)  # -1 is the first element in the opposite side of the vector
+    for obs in range(len(obsPos[:])):
+        d = ca.sqrt((obsPos[obs][0] - p_x)**2 + (obsPos[obs][1] - p_y)**2)
+        potential = ca.sqrt(1 / (1 + ca.exp(-20 * (d - obsPos[obs][2]))))
+        opti.subject_to(potential.T[0:-1]*d[0:-1].T >= s + obsPos[obs][2])  # -1 is the first element in the opposite side of the vector
 
-    opti.minimize(3*ca.sumsqr(e_x) + 3*ca.sumsqr(e_y) + 0.01*ca.sumsqr(U) + 1000*ca.sumsqr(s))
+    opti.minimize(2*ca.sumsqr(e_x) + 2*ca.sumsqr(e_y) + 0.01*ca.sumsqr(U) + 100*ca.sumsqr(s))
 
     # Use interior point optimization
     opti.solver('ipopt', {'ipopt': {'print_level': 0}})
-
+    # Set initial conditions
     opti.set_value(x0, [0, 0, 0])
     sol = opti.solve()
 
@@ -145,40 +145,47 @@ def main():
 
     sol_traj = pd.DataFrame(np.hstack((x_traj, u_traj)), columns=['x', 'y', 'theta', 'v', 'omega'])
 
+    t2 = time.time()
+    print(t2-t1)
+
     # Plot the trajectory around the obstacle
-    # plt.plot(2*sol.value(e_x[0:-1])**2 + 2*sol.value(e_y[0:-1])**2 + 3000*sol.value(s)**2)
+    plt.plot(2*sol.value(e_x[0:-1])**2 + 2*sol.value(e_y[0:-1])**2 + 3000*sol.value(s)**2)
 
-    #fig, ax = plt.subplots(figsize=(8, 8))
+    fig, ax = plt.subplots(figsize=(8, 8))
+    c = np.zeros(len(obsPos[:]))
+    for obs in range(len(obsPos[:])):
+        c = plt.Circle((obsPos[obs][0], obsPos[obs][1]), radius=obsPos[obs][2], alpha=0.5)
+        ax.add_patch(c)
 
-    #c = plt.Circle((obsPos[0], obsPos[1]), radius=obsRadius, alpha=0.5)
+    ax.plot(goal[0], goal[1], 'x', markersize=20)
+    ax.plot(sol_traj["x"], sol_traj["y"])
+    ax.set_xlim([-1, 1])
+    ax.set_ylim([-1, 1])
+    ax.grid()
+    print(opti)
 
-    #ax.plot(goal[0], goal[1], 'x', markersize=20)
-    #ax.plot(sol_traj["x"], sol_traj["y"])
-    #ax.add_patch(c)
-
-    #ax.set_xlim([-1, 1])
-    #ax.set_ylim([-1, 1])
+    plt.show()
 
     #while True:
 
-    pathmsg = getGlobalPlan()
-    xpos = []
-    ypos = []
-    for i in range(len(pathmsg.poses)):
-        xpos.append(pathmsg.poses[i].pose.position.x)
-        ypos.append(pathmsg.poses[i].pose.position.y)
-    fig, ax = plt.subplots(figsize=(8, 8))
-    ax.set_xlim([-2.5, 1])
-    ax.set_ylim([-2.5, 1])
-    ax.grid()
-    ax.plot(xpos, ypos)
+    #pathmsg = getGlobalPlan()
+    #xpos = []
+    #ypos = []
+    #for i in range(len(pathmsg.poses)):
+    #    xpos.append(pathmsg.poses[i].pose.position.x)
+    #    ypos.append(pathmsg.poses[i].pose.position.y)
+    #fig, ax = plt.subplots(figsize=(8, 8))
+    #ax.set_xlim([-2.5, 1])
+    #ax.set_ylim([-2.5, 1])
+    #ax.grid()
+    #ax.plot(xpos, ypos)
 
 
-    h = 200 # This is the horizon/point along the global plan which will be used as a goal
-    goal = [pathmsg.poses[h].pose.position.x, pathmsg.poses[h].pose.position.y]
-    ax.plot(goal[0], goal[1], 'x', markersize=20)
-    ax.text(goal[0]+0.1, goal[1]+0.1, 'Goal', style='oblique', fontsize=14)
-    plt.show()
+    #h = 200 # This is the horizon/point along the global plan which will be used as a goal
+    #goal = [pathmsg.poses[h].pose.position.x, pathmsg.poses[h].pose.position.y]
+    #ax.plot(goal[0], goal[1], 'x', markersize=20)
+    #ax.text(goal[0]+0.1, goal[1]+0.1, 'Goal', style='oblique', fontsize=14)
+    #plt.show()
 
 if __name__=="__main__":
     main()

168cb969e01f99269ab63fbd752f7416d1c76dae
rejzi <reiserbalazs@gmail.com>
2020-05-08 16:24:35 +0200

    Clear up MPC_Diff_drive folder

--- MPC_Differential_Drive/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/martinpc/PycharmProjects/TIAGo-hamr-navigation/MPC_Differential_Drive/src
-# Build directory: /home/martinpc/PycharmProjects/TIAGo-hamr-navigation/MPC_Differential_Drive/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(gtest)

bf7e084a1a60b157c83e012620496662560cd5f1
rejzi <reiserbalazs@gmail.com>
2020-06-01 15:56:35 +0200

    current so fixed, frame transpose fixed, everything merged

--- MPC_Differential_Drive/src/CasadiMPC_PolygonAvoidance.py
+++ MPC_Differential_Drive/src/CasadiMPC_PolygonAvoidance.py
@@ -6,11 +6,7 @@ import numpy as np
 import numpy.matlib
 # ROS specific modules
 import rospy
-from geometry_msgs.msg import Twist
 from geometry_msgs.msg import *
-from geometry_msgs.msg import Point32, Point
-from geometry_msgs.msg import PoseWithCovarianceStamped
-from geometry_msgs.msg import PoseStamped, PoseArray, Pose
 from visualization_msgs.msg import MarkerArray, Marker
 from costmap_converter.msg import ObstacleArrayMsg, ObstacleMsg
 
@@ -25,12 +21,15 @@ class CasadiMPC:
         self.pub_prediction_poses = rospy.Publisher('/prediction_poses', PoseArray, queue_size=0)
         self.pub_goal = rospy.Publisher('/local_goal', PoseStamped, queue_size=0)
         self.pub_mo_viz = rospy.Publisher('/mobs', MarkerArray, queue_size=0)
-        self.pub_cl_ploy = rospy.Publisher('/cl_polygons', MarkerArray, queue_size=0)
+        self.pub_current_obs = rospy.Publisher('/current_obs', MarkerArray, queue_size=0)
         #subscribers
         self.robotpose_sub = rospy.Subscriber('/robot_pose', PoseWithCovarianceStamped, self.callback_pose)
         self.goal_sub = rospy.Subscriber('/goal_pub', PoseStamped, self.callback_goal)
         self.costmap_converter_sub = rospy.Subscriber('/costmap_converter/costmap_obstacles', ObstacleArrayMsg, self.callback_so)
         self.mo_sub = rospy.Subscriber('/MO_Obstacles', ObstacleArrayMsg, self.callback_mo)
+        #Markers
+        self.MOMarkerArray = MarkerArray()
+        self.SOMarkerArray = MarkerArray()
         #casadi variables
         self.lbw = np.array(lbw)
         self.ubw = np.array(ubw)
@@ -46,31 +45,32 @@ class CasadiMPC:
         self.mpc_i = 0
         self.goal_tolerance = [0.2, 0.2]
         self.tf = TransformListener()
+        self.cl_obs = []
+        self.cl_sizes = []
 
     def callback_goal(self, goal_data):  # Current way to get the goal
-        yaw_goal = quaternion_to_euler(goal_data.pose.orientation.x, goal_data.pose.orientation.y, goal_data.pose.orientation.z, goal_data.pose.orientation.w)
+        yaw_goal = quaternion_to_euler(goal_data.pose.orientation.x, goal_data.pose.orientation.y,
+                                       goal_data.pose.orientation.z, goal_data.pose.orientation.w)
         self.goal = np.array(([goal_data.pose.position.x], [goal_data.pose.position.y], [yaw_goal]))
         goal_data.header.stamp = rospy.Time.now()
         goal_data.header.frame_id = "/map"
         self.pub_goal.publish(goal_data)
 
     def callback_pose(self, pose_data):  # Update the pose either using the topics robot_pose or amcl_pose.
-        yaw_pose = quaternion_to_euler(pose_data.pose.pose.orientation.x, pose_data.pose.pose.orientation.y, pose_data.pose.pose.orientation.z, pose_data.pose.pose.orientation.w)
+        yaw_pose = quaternion_to_euler(pose_data.pose.pose.orientation.x, pose_data.pose.pose.orientation.y,
+                                       pose_data.pose.pose.orientation.z, pose_data.pose.pose.orientation.w)
         self.pose = np.array(([pose_data.pose.pose.position.x], [pose_data.pose.pose.position.y], [yaw_pose]))
 
     def callback_so(self, obs_data):
-        obs_data.header.frame_id = '/odom'
-
+        obs_data.header.frame_id = "odom"
         for i in range(len(obs_data.obstacles)):
-
             pt = PointStamped()
-            pt.header.frame_id = obs_data.header.frame_id
-
+            pt.header.frame_id = "odom"
             for k in range(len(obs_data.obstacles[i].polygon.points)):
                 pt.point.x = obs_data.obstacles[i].polygon.points[k].x
                 pt.point.y = obs_data.obstacles[i].polygon.points[k].y
                 pt.point.z = obs_data.obstacles[i].polygon.points[k].z
-                tfpts = self.tf.transformPoint("/map", pt)
+                tfpts = self.tf.transformPoint("map", pt)
                 obs_data.obstacles[i].polygon.points[k] = tfpts.point
         self.SO_obs = obs_data
 
@@ -83,8 +83,8 @@ class CasadiMPC:
         if self.goal is not None and self.MO_obs is not None and self.SO_obs is not None:
             x0 = self.pose
             x_goal = self.goal
-            #print('This is the GOAL: ', x_goal)
-            #print('This is the ROBOT POSE:', x0)
+            print('This is the GOAL: ', x_goal)
+            print('This is the ROBOT POSE:', x0)
 
             self.p[0:6] = np.append(x0, x_goal)
 
@@ -94,13 +94,15 @@ class CasadiMPC:
                 for i in range(n_MO):
                     self.p[i_pos + 2:i_pos + 5] = np.array([self.MO_obs[i].velocities.twist.linear.x, self.MO_obs[i].orientation.z, self.MO_obs[i].radius])
                     t_predicted = k*Ts
-                    obs_x = self.MO_obs[i].polygon.points[0].x + t_predicted * self.MO_obs[i].velocities.twist.linear.x * ca.cos(self.MO_obs[i].orientation.z)
-                    obs_y = self.MO_obs[i].polygon.points[0].y + t_predicted * self.MO_obs[i].velocities.twist.linear.x * ca.sin(self.MO_obs[i].orientation.z)
+                    obs_x = self.MO_obs[i].polygon.points[0].x + t_predicted*self.MO_obs[i].velocities.twist.linear.x*ca.cos(self.MO_obs[i].orientation.z)
+                    obs_y = self.MO_obs[i].polygon.points[0].y + t_predicted*self.MO_obs[i].velocities.twist.linear.x*ca.sin(self.MO_obs[i].orientation.z)
                     self.p[i_pos:i_pos + 2] = [obs_x, obs_y]
                     i_pos += 5
 
             #SO constraints
             self.p[i_pos:i_pos+n_SO*7] = find_closest_n_so(self.SO_obs, x0, n_SO)
+            self.cl_obs = self.p[i_pos:i_pos+n_SO*6]
+            self.cl_sizes = self.p[i_pos+n_SO*6:i_pos+n_SO*7]
 
             x0k = np.append(self.x_st_0.reshape(3 * (N + 1), 1), self.u0.reshape(2 * N, 1))
             x0k = x0k.reshape(x0k.shape[0], 1)
@@ -119,52 +121,98 @@ class CasadiMPC:
             cmd_vel = Twist()
             cmd_vel.linear.x = u_sol[0]
             cmd_vel.angular.z = u_sol[1]
-            #print('APPLIED CMD_VEL:', cmd_vel)
+            print('------------')
+            print('APPLIED CMD_VEL:', cmd_vel)
             self.pub_nav_vel.publish(cmd_vel)
 
             self.mpc_i = self.mpc_i + 1
 
-            # Publish obstacles and states to Rviz
-            moArray = MarkerArray()
-            for i in range(n_MO):
-                marker = Marker()
-                marker.id = i
-                marker.header.frame_id = '/map'
-                marker.header.stamp = rospy.Time.now()
-                marker.type = marker.CYLINDER
-                marker.action = marker.ADD
-                marker.scale.x = 2 * self.MO_obs[i].radius
-                marker.scale.y = 2 * self.MO_obs[i].radius
-                marker.scale.z = 0.75
-                marker.color.r = 1.0
-                marker.color.g = 1.0
-                marker.color.b = 1.0
-                marker.color.a = 1.0
-                #marker.pose.position.x = self.MO_obs[i].polygon.points[0].x
-                #marker.pose.position.y = self.MO_obs[i].polygon.points[0].y
-                #marker.pose.position.z = 0
-                #marker.pose.orientation.w = 1.0
-                moArray.markers.append(marker)
-            self.pub_mo_viz.publish(moArray)
-
-            poseArray = PoseArray()
-            poseArray.header.stamp = rospy.Time.now()
-            poseArray.header.frame_id = "/map"
-            for k in range(len(self.x_st_0)):
-                x_st = Pose()
-                x_st.position.x = self.x_st_0[k, 0]
-                x_st.position.y = self.x_st_0[k, 1]
-                [qx, qy, qz, qw] = euler_to_quaternion(0, 0, self.x_st_0[k, 2])
-                x_st.orientation.x = qx
-                x_st.orientation.y = qy
-                x_st.orientation.z = qz
-                x_st.orientation.w = qw
-                poseArray.poses.append(x_st)
-            self.pub_prediction_poses.publish(poseArray)
+            # Publish to Rviz
+            self.publish_mo_marker()
+            self.publish_mpc_prediction()
+            self.publish_current_so()
 
         else:
-            print("Waiting for pathsplit, MO_publisher or costmap_converter_publisher")
+            print("Waiting for pathsplit, MO_publisher or constmap_converter_publisher")
 
+    def publish_mo_marker(self):
+        self.MOMarkerArray = MarkerArray()
+        for i in range(n_MO):
+            marker = Marker()
+            marker.id = i
+            marker.header.frame_id = '/map'
+            marker.header.stamp = rospy.Time.now()
+            marker.type = marker.CYLINDER
+            marker.action = marker.ADD
+            marker.scale.x = 2 * self.MO_obs[i].radius
+            marker.scale.y = 2 * self.MO_obs[i].radius
+            marker.scale.z = 0.3
+            marker.color.r = 1.0
+            marker.color.g = 1.0
+            marker.color.b = 0.0
+            marker.color.a = 1.0
+            marker.pose.position.x = self.MO_obs[i].polygon.points[0].x
+            marker.pose.position.y = self.MO_obs[i].polygon.points[0].y
+            marker.pose.position.z = 0
+            marker.pose.orientation.w = 1.0
+            self.MOMarkerArray.markers.append(marker)
+        self.pub_mo_viz.publish(self.MOMarkerArray)
+
+    def publish_mpc_prediction(self):
+        poseArray = PoseArray()
+        poseArray.header.stamp = rospy.Time.now()
+        poseArray.header.frame_id = "/map"
+        for k in range(len(self.x_st_0)):
+            x_st = Pose()
+            x_st.position.x = self.x_st_0[k, 0]
+            x_st.position.y = self.x_st_0[k, 1]
+            [qx, qy, qz, qw] = euler_to_quaternion(0, 0, self.x_st_0[k, 2])
+            x_st.orientation.x = qx
+            x_st.orientation.y = qy
+            x_st.orientation.z = qz
+            x_st.orientation.w = qw
+            poseArray.poses.append(x_st)
+        self.pub_prediction_poses.publish(poseArray)
+
+    def publish_current_so(self):
+        point_step = 0
+        obs_array = []
+        for k in range(n_SO):
+            temp_obs = self.cl_obs[point_step:point_step+2*(int(self.cl_sizes[k]))]
+            print('This is the temp_obs: ', temp_obs)
+            obs_array.append(temp_obs)
+            point_step += 6
+
+        self.SOMarkerArray = MarkerArray()
+        for k in range(n_SO):
+            marker = Marker()
+            marker.id = k
+            marker.header.frame_id = 'map'
+            marker.header.stamp = rospy.Time.now()
+            marker.type = marker.LINE_STRIP
+            marker.action = marker.ADD
+            marker.scale.x = 0.1
+            marker.color.r = 1.0
+            marker.color.g = 0.0
+            marker.color.b = 0.0
+            marker.color.a = 1.0
+            marker.points = []
+            point_step = 0
+            for i in range(int(self.cl_sizes[k])):
+                temp_point = Point32()
+                temp_point.x = obs_array[k][point_step]
+                temp_point.y = obs_array[k][point_step+1]
+                temp_point.z = 0
+                point_step += 2
+                marker.points.append(temp_point)
+                if int(self.cl_sizes[k]) == 3:
+                    temp_point = Point32()
+                    temp_point.x = obs_array[k][point_step-6]
+                    temp_point.y = obs_array[k][point_step-5]
+                    temp_point.z = 0
+                    marker.points.append(temp_point)
+            self.SOMarkerArray.markers.append(marker)
+        self.pub_current_obs.publish(self.SOMarkerArray)
 
 def poligon2centroid(SO_data):
 
@@ -234,9 +282,9 @@ def poligon2centroid(SO_data):
         return np.array([centroid_x, centroid_y])
 
 def find_closest_n_so(SO_data, pose, n_SO):
-
     # This function finds the closest n_SO number of obstacles.
-    # If there are less measured obstacle than n_SO append extra far away obstacles to the SO data
+
+    #If there are less measured obstacle than n_SO append extra far away obstacles to the SO data
     if len(SO_data.obstacles[:]) < n_SO:
         for i in range(len(SO_data.obstacles[:]), n_SO+1):
             fill_obs = ObstacleMsg()
@@ -248,14 +296,14 @@ def find_closest_n_so(SO_data, pose, n_SO):
     dist = np.zeros((1, len(SO_data.obstacles[:])))
 
     for k in range(len(SO_data.obstacles[:])):
-        print('This is for point {}'.format(k), SO_data.obstacles[k].polygon.points[:])
+        #print('This is for point {}'.format(k), SO_data.obstacles[k].polygon.points[:])
         #print('This is for point {}'.format(k), len(SO_data.obstacles[k].polygon.points[:]))
 
         [x, y] = poligon2centroid(SO_data.obstacles[k].polygon.points[:])
-        print('This is centroid x and y: ', [x, y])
+        #print('This is centroid x and y: ', [x, y])
 
         dist[0, k] = np.sqrt((pose[0]-x) ** 2 + (pose[1]-y) ** 2)
-        print('This is the dist: ', dist[0, k])
+        #print('This is the dist: ', dist[0, k])
 
     # Indexes of the closest Static obstacles
     n_idx = (dist).argsort()[:n_SO]
@@ -280,7 +328,8 @@ def find_closest_n_so(SO_data, pose, n_SO):
                 cl_obs[k*6:k*6+6] = np.array([SO_now[0].x, SO_now[0].y, SO_now[1].x, SO_now[1].y, 0, 0])
                 cl_sizes[k] = 2
             else:
-                cl_obs[k*6:k*6+6] = np.array([SO_now[0].x, SO_now[0].y, SO_now[1].x, SO_now[1].y, SO_now[2].x, SO_now[2].y])
+                cl_obs[k*6:k*6+6] = np.array([SO_now[0].x, SO_now[0].y, SO_now[1].x,
+                                              SO_now[1].y, SO_now[2].x, SO_now[2].y])
                 cl_sizes[k] = 3
         else:
             cl_sizes[k] = 3
@@ -296,60 +345,9 @@ def find_closest_n_so(SO_data, pose, n_SO):
     #print('This is the shape of cl_obs', cl_obs.shape)
     #print('cl_sizes', cl_sizes)
     combined_obs_and_size = np.append(cl_obs, cl_sizes)
-
-
-    pub5 = rospy.Publisher('/current_obs', MarkerArray, queue_size=0)
-    point_step = 0
-    obs_array = []
-    for obs in range(len(cl_sizes)):
-        temp_obs = cl_obs[point_step:point_step+2*(int(cl_sizes[obs]))]
-        #print('This is the temp_obs: ', temp_obs)
-        obs_array.append(temp_obs)
-        point_step += 6
-
-    #obs_array = np.asarray(obs_array)
-    #print('This is the first obstacle in obs_array: ', obs_array[0])
-    #print('This is the first entry in the first obstacle: ', obs_array[0][0])
-
-    obsArray = MarkerArray()
-    for k in range(n_SO):
-        marker = Marker()
-        marker.id = k
-        marker.header.frame_id = '/odom'
-        marker.header.stamp = rospy.Time.now()
-        marker.type = marker.LINE_STRIP
-        marker.action = marker.ADD
-        marker.scale.x = 0.1
-        marker.scale.y = 0.1
-        marker.scale.z = 0.1
-        marker.color.r = 1.0
-        marker.color.g = 0.0
-        marker.color.b = 0.0
-        marker.color.a = 1.0
-        #marker.pose.position.x = obs_array[k][0]
-        #marker.pose.position.y = obs_array[k][1]
-        #marker.pose.position.z = 0
-        #marker.pose.orientation.w = 1.0
-        marker.points = []
-        point_step = 0
-        for i in range(int(cl_sizes[k])):
-            temp_point = Point32()
-            temp_point.x = obs_array[k][point_step]
-            temp_point.y = obs_array[k][point_step+1]
-            #print('This is the temp_point: ', temp_point)
-            point_step += 2
-            marker.points.append(temp_point)
-        temp_point = Point32()
-        temp_point.x = obs_array[k][0]
-        temp_point.y = obs_array[k][1]
-        marker.points.append(temp_point)
-        obsArray.markers.append(marker)
-    #print('This is obsArray: ', obsArray)
-    pub5.publish(obsArray)
     return combined_obs_and_size
 
 def euler_to_quaternion(roll, pitch, yaw):
-
         qx = np.sin(roll/2) * np.cos(pitch/2) * np.cos(yaw/2) - np.cos(roll/2) * np.sin(pitch/2) * np.sin(yaw/2)
         qy = np.cos(roll/2) * np.sin(pitch/2) * np.cos(yaw/2) + np.sin(roll/2) * np.cos(pitch/2) * np.sin(yaw/2)
         qz = np.cos(roll/2) * np.cos(pitch/2) * np.sin(yaw/2) - np.sin(roll/2) * np.sin(pitch/2) * np.cos(yaw/2)
@@ -357,7 +355,6 @@ def euler_to_quaternion(roll, pitch, yaw):
         return [qx, qy, qz, qw]
 
 def quaternion_to_euler(x, y, z, w):
-
     t0 = +2.0 * (w * x + y * z)
     t1 = +1.0 - 2.0 * (x * x + y * y)
     #roll = math.atan2(t0, t1) # Not used currently
@@ -371,19 +368,16 @@ def quaternion_to_euler(x, y, z, w):
     return yaw
 
 def if_poly_line(const_vect, poly, posex, posey, keep_dist):
-
     closest_point = find_closest_point_on_line(posex, posey, poly[0], poly[1], poly[2], poly[3])
     const_vect = ca.vertcat(const_vect, -ca.sqrt((posex-closest_point[0])**2+(posey-closest_point[1])**2) + keep_dist)
     return const_vect
 
 def if_poly_triangle(const_vect, poly, posex, posey, keep_dist):
-
     closest_point = find_closest_point_to_triangle(poly[0:6], posex, posey)
     const_vect = ca.vertcat(const_vect, -ca.sqrt((posex-closest_point[0])**2+(posey-closest_point[1])**2) + keep_dist)
     return const_vect
 
 def find_closest_point_on_line(posex, posey, startx, starty, endx, endy):
-
     #A to Position = position - line_start
     a2px = posex - startx
     a2py = posey - starty
@@ -400,7 +394,6 @@ def find_closest_point_on_line(posex, posey, startx, starty, endx, endy):
     return closest_point
 
 def find_closest_point_to_triangle(poly, posex, posey):
-
     point1 = find_closest_point_on_line(posex, posey, poly[0], poly[1], poly[2], poly[3])
     point2 = find_closest_point_on_line(posex, posey, poly[2], poly[3], poly[4], poly[5])
     point3 = find_closest_point_on_line(posex, posey, poly[4], poly[5], poly[0], poly[1])
@@ -418,23 +411,24 @@ if __name__ == '__main__':
     rospy.init_node('casadi_tiago_mpc', anonymous=True)
 
     # MPC Parameters
-    Ts = 0.1  # Timestep
+    Ts = 0.1  # TimeStep
     N = 20  # Horizon
+
+    # Obstacle Parameters
     n_SO = 10
+    n_MO = 4
+    n_MOst = 5
 
     # Robot Parameters
     safety_boundary = 0.15
     rob_diameter = 0.54
-    v_max = 1  # m/s
-    v_min = 0  # -v_max
+    v_max = 0.5  # m/s
+    v_min = 0 # -v_max
     w_max = ca.pi/2  # rad/s
     w_min = -w_max
-    acc_v_max = 1  # m/ss
-    acc_w_max = ca.pi  # / 2  # rad/ss
+    acc_v_max = 0.4  # m/ss
+    acc_w_max = ca.pi / 4  # rad/ss
 
-    # Obstacle Parameters
-    n_MO = 4
-    n_MOst = 5
 
     # System Model
     x = ca.SX.sym('x')
@@ -532,16 +526,16 @@ if __name__ == '__main__':
         for i in range(n_SO):
             const_vect = ca.if_else(PolyDims[i] < 3,
                                     ca.if_else(PolyDims[i] < 2,
-                                    ca.vertcat(const_vect, -ca.sqrt((X[0,k]-PolyXY[k_pos])**2+(X[1,k]-PolyXY[k_pos+1])**2) + keep_dist),
+                                    ca.vertcat(const_vect, -ca.sqrt((X[0,k]-PolyXY[k_pos])**2 +
+                                                                    (X[1,k]-PolyXY[k_pos+1])**2) + keep_dist),
                                     if_poly_line(const_vect, PolyXY[k_pos:k_pos+4], X[0,k], X[1,k], keep_dist)),
                                     if_poly_triangle(const_vect, PolyXY[k_pos:k_pos+6], X[0,k], X[1,k], keep_dist))
             k_pos = k_pos+6
         print('Paramater matrix in {} prediction is calculated'.format(k+1))
 
-
-# Non-linear programming setup
+    # Non-linear programming setup
     OPT_variables = ca.vertcat(ca.reshape(X, 3 * (N + 1), 1),
-                               ca.reshape(U, 2 * N, 1))  # Single shooting, create a vector from U [v,w,v,w,...]
+                               ca.reshape(U, 2 * N, 1))
 
     nlp_prob = {'x': OPT_variables,
                 'f': obj,
